6e131637a4fee15a312b93f91b0fdcd748397043
Fatih Yavuz <yvzfth@yandex.com>
2026-08-30 12:24:56 +0300

    fix: update base path in Vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "./",
+  base: '/me/',
   server: {
     port: 5173,
     open: true,
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     sourcemap: false,
   },
 });
